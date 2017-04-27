26b4501788ead5775626572da561d4b5527e09be
qingwang175 <qingwang@utexas.edu>
2017-04-27 00:20:00 -0500

    Merge Conflict start

--- Ootd/app/src/main/java/com/qinglenmeson/ootd/Day.java
+++ Ootd/app/src/main/java/com/qinglenmeson/ootd/Day.java
@@ -1,5 +1,29 @@
 package com.qinglenmeson.ootd;
 
+import android.location.Location;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import static android.R.attr.data;
+
+import java.util.Date;
+import java.time.*;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Locale;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+
 /**
  * Created by James Park on 4/25/2017.
  */
@@ -7,13 +31,53 @@ package com.qinglenmeson.ootd;
 public class Day {
     int month;
     int day;
-    int temperature;
+    int tempHigh;
+    int tempLow;
     boolean rain;
 
-    public void day(int m, int d)
+
+
+    public void day()
     {
-        month = m;
-        day = d;
-        //call weather api to set temperature and rain;
+        //set month and day varibles to current month and day
+        Calendar cal = Calendar.getInstance();
+        this.month = cal.get(Calendar.DAY_OF_MONTH);
+        this.day = cal.get(Calendar.MONTH);
+
+        //parse weather data
+        JSONObject jObj = new JSONObject(getWeather());
+
+        // We start extracting the info
+        Location loc = new Location();
+
+        JSONObject coordObj = getObject("coord", jObj);
+        loc.setLatitude(getFloat("lat", coordObj));
+        loc.setLongitude(getFloat("lon", coordObj));
+
+        JSONObject sysObj = getObject("sys", jObj);
+        loc.setCountry(getString("country", sysObj));
+        loc.setSunrise(getInt("sunrise", sysObj));
+        loc.setSunset(getInt("sunset", sysObj));
+        loc.setCity(getString("name", jObj));
+        weather.location = loc;
+    }
+
+    private String getWeather(){
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://samples.openweathermap.org/data/2.5/weather?zip=78705,us&appid=ab6c9e595fe584dc2765d622f3b62da0")
+                .build();
+        try{
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+
+
     }
 }
--- Ootd/app/src/main/java/com/qinglenmeson/ootd/Outfit.java
+++ Ootd/app/src/main/java/com/qinglenmeson/ootd/Outfit.java
@@ -1,14 +1,13 @@
 package com.qinglenmeson.ootd;
 
 import java.util.ArrayList;
+import java.math.*;
 
 /**
  * Created by James Park on 4/25/2017.
  */
 
 public class Outfit {
-
-
 
     public void Outfit(){
 
@@ -17,6 +16,25 @@ public class Outfit {
     public static Outfit generate(ArrayList<Clothing> clothes, Day day){
         Outfit outfit = new Outfit();
 
+        if (day.rain == true){
+            //rain jacket
+        }
+        else {//no rain
+            if ((day.tempHigh - day.tempLow) >= 30) {
+                if (day.tempHigh >= 70) {
+                    //layers
+                }
+            } else {//no layers
+                //no layers
+            }
+        }
         return outfit;
     }
+
+    private int randomNum(int min, int max){
+        int random = (int )(Math.random() * max + min);
+        return random;
+    }
+
+
 }
